248750cda57d95fe110a7cc76970ace7caccb2dc
Vlaicu329 <123724308+Vlaicu329@users.noreply.github.com>
2023-05-16 18:45:23 +0200

    Codigo primario

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese su nombre: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("¡Hola " + name + "!");
+    }
+}
